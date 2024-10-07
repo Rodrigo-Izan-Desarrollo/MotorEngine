d24203992eb608ef1e60830357a42c67d4ba7111
izansl <125253405+izansl@users.noreply.github.com>
2024-10-07 17:53:05 +0200

    Template

---
+++ sdl2_simple_example/sdl2_simple_example/MyWindow.cpp
@@ -0,0 +1,94 @@
+#include <exception>
+#include <SDL2/SDL_video.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_opengl.h>
+#include "MyWindow.h"
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_opengl3.h"
+#include <SDL2/SDL.h>
+
+using namespace std;
+
+
+MyWindow::MyWindow(const char* title, unsigned short width, unsigned short height){
+	open(title, width, height);
+
+}
+
+MyWindow::~MyWindow() {
+    close();
+    
+}
+
+void MyWindow::open(const char* title, unsigned short width, unsigned short height) {
+    if (isOpen()) return;
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    _window = SDL_CreateWindow( title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
+    if (!_window) throw exception(SDL_GetError());
+
+    _ctx = SDL_GL_CreateContext(_window);
+    if (!_ctx) throw exception(SDL_GetError());
+    if (SDL_GL_MakeCurrent(_window, _ctx) != 0) throw exception(SDL_GetError());
+    if (SDL_GL_SetSwapInterval(1) != 0) throw exception(SDL_GetError());
+
+    ImGui::CreateContext();
+    ImGui_ImplSDL2_InitForOpenGL(_window, _ctx);
+    ImGui_ImplOpenGL3_Init("#version 130");
+}
+
+
+
+void MyWindow::swapBuffers() const {
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
+
+    if (ImGui::BeginMainMenuBar()) {
+        if (ImGui::BeginMenu("Menu")) {
+            if (ImGui::MenuItem("Adeu")) {
+                SDL_Event quit_event;
+                quit_event.type = SDL_QUIT;
+                SDL_PushEvent(&quit_event);
+            }
+            ImGui::EndMenu();
+        }
+        ImGui::EndMainMenuBar();
+    }
+
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    SDL_GL_SwapWindow(static_cast<SDL_Window*>(_window));
+}
+
+
+ bool MyWindow ::processEvents(IEventProcessor* eventp)
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        if (eventp) eventp->processEvent(event);
+        switch (event.type) {
+        case SDL_QUIT:
+            return false;
+            break;
+        default:
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            break;
+        }
+    }
+    return true;
+}
+
+
+
+ void MyWindow::close()
+ {
+     
+ }
